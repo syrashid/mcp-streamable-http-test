5169384f0222a758a400c32e0f61c2a4db9e9703
alejandro <dev@alejandro-ao.com>
2025-06-22 21:23:39 +0200

    fix port

--- fastapi_example/server.py
+++ fastapi_example/server.py
@@ -17,6 +17,8 @@ app = FastAPI(lifespan=lifespan)
 app.mount("/echo", echo_mcp.streamable_http_app())
 app.mount("/math", math_mcp.streamable_http_app())
 
+PORT = os.environ.get("PORT", 10000)
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
